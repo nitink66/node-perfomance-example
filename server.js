12cebfc76b25aa603fa5aced6a61f23e8eedf369
Nitin K <nitink61996@gmail.com>
2022-07-30 12:44:06 +0530

    created multiple instances on node to handle blocking requests

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cluster = require('cluster');
 
 const app = express();
 
@@ -13,14 +14,30 @@ function delay(delayInMs) {
 }
 
 app.get('/', (req, res) => {
-    res.send('Main Route :)');
+    res.send(`Main Route ${process.pid}`);
 });
 
 app.get('/timer', (req, res) => {
     delay(9000);
-    res.send('Timer executed');
+    res.send(`Timer executed ${process.pid}`);
 });
 
-app.listen(PORT, () => {
-    console.log(`Started on PORT : ${PORT}`);
-});
+console.log('Running server.js...');
+
+// since event loop gets blocked ,
+// Clusters of Node.js processes can be used to run multiple
+// instances of Node.js that can distribute workloads among their application threads
+
+if (cluster.isMaster) {
+    console.log('Master Process has been started');
+    // fork creates workers
+    cluster.fork();
+    cluster.fork();
+} else {
+    // divide incoming requests with worker
+    //  workers have same server code
+    console.log('Worker Process has been started');
+    app.listen(PORT, () => {
+        console.log(`Started on PORT : ${PORT}`);
+    });
+}
