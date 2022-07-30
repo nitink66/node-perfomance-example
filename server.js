a55aa408e2dc68f6c78a85995dcc275b825e7d7a
Nitin K <nitink61996@gmail.com>
2022-07-30 21:58:21 +0530

    Added PM2 to handle clusters

--- server.js
+++ server.js
@@ -1,6 +1,4 @@
 const express = require('express');
-const cluster = require('cluster');
-const os = require('os');
 
 const app = express();
 
@@ -19,29 +17,31 @@ app.get('/', (req, res) => {
 });
 
 app.get('/timer', (req, res) => {
-    delay(9000);
+    delay(4000);
     res.send(`Timer executed ${process.pid}`);
 });
-
-console.log('Running server.js...');
 
 // since event loop gets blocked ,
 // Clusters of Node.js processes can be used to run multiple
 // instances of Node.js that can distribute workloads among their application threads
 
-if (cluster.isMaster) {
-    console.log('Master Process has been started');
-    // fork creates workers
-    // maximizing the performance of our server based on the amount of CPU cores in our machine.
-    const NUM_OF_WORKERS = os.cpus().length;
-    for (i = 0; i < NUM_OF_WORKERS; i++) {
-        cluster.fork();
-    }
-} else {
-    // divide incoming requests with worker
-    //  workers have same server code
-    console.log('Worker Process has been started');
-    app.listen(PORT, () => {
-        console.log(`Started on PORT : ${PORT}`);
-    });
-}
+// if (cluster.isMaster) {
+//     console.log('Master Process has been started');
+//     // fork creates workers
+//     // maximizing the performance of our server based on the amount of CPU cores in our machine.
+//     const NUM_OF_WORKERS = os.cpus().length;
+//     for (i = 0; i < NUM_OF_WORKERS; i++) {
+//         cluster.fork();
+//     }
+// } else {
+//     // divide incoming requests with worker
+//     //  workers have same server code
+//     console.log('Worker Process has been started');
+//     app.listen(PORT, () => {
+//         console.log(`Started on PORT : ${PORT}`);
+//     });
+// }
+
+app.listen(PORT, () => {
+    console.log(`Started on PORT : ${PORT}`);
+});
